7825783d7835c77a7b79d6e32d44e3fa71fadf44
alireza.ghazanfari <alireza.ghazanfari@digikala.com>
2022-08-05 22:10:07 +0430

    write output

--- compiler/main.py
+++ compiler/main.py
@@ -1,10 +1,10 @@
 import logging
-
+from compiler.ply_2_lark_converter import gen_lark_tree
 from compiler.preprocessor import run_preprocess
 from compiler.yacc import parser
+from compiler.gen_code import generate_code
 
-
-def run(input_file_address: str) -> bool:
+def run(input_file_address: str) -> str:
     result = ''
     with open(input_file_address) as input_file:
         data = input_file.read()
@@ -14,9 +14,12 @@ def run(input_file_address: str) -> bool:
     )
     log = logging.getLogger()
     try:
-        res = parser.parse(input=data, debug=True)
-        # return True
-        return "OK"
+        tree_data = parser.parse(input=data, debug=True)
+        tree = gen_lark_tree(tree_data)
+        result = generate_code(tree)
+        output_file = open("../tmp/res.mips", "w")
+        output_file.write(result)
+        return "Successful"
     except SyntaxError as e:
         return "Syntax Error"
         #return False
