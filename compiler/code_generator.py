65570031e33801cda5fbec4f6e4c149ee789d2fb
seyyedAlirezaGhazanfari <59168138+seyyedAlirezaGhazanfari@users.noreply.github.com>
2022-08-07 16:40:12 +0430

    feat(cgen): add generate method basis

--- compiler/code_generator.py
+++ compiler/code_generator.py
@@ -1,5 +1,38 @@
-def generate(code):
+from lark import Lark, ParseError
+from lark.visitors import Interpreter
+from compiler.mips_codes import MIPS
+from compiler.decaf_enums import Constants
+from compiler.semantic_error import SemanticError
+from compiler.symbol_table import Variable, SymbolTable, Type
+
+stack = []
+
+
+class CodeGenerator(Interpreter):
     pass
+
+
+def prepare_main_tree(tree):
+    tree.symbol_table = SymbolTable()
+    tree.symbol_table.add_type(Type('int', 4))
+    tree.symbol_table.add_type(Type('double', 8))
+    tree.symbol_table.add_type(Type('bool', 4))
+    tree.symbol_table.add_type(Type('void', 0))
+    tree.symbol_table.add_type(Type('string', 8))
+    tree.symbol_table.add_type(Type('array', 4))
+
+
+def generate(input_code):
+    parser = Lark.open('./grammar.lark', parser="lalr", propagate_positions=True)
+    try:
+        tree = parser.parse(input_code)
+        prepare_main_tree(tree)
+        mips_code = CodeGenerator.visit(tree)
+    except ParseError as e:
+        return e
+    except SemanticError:
+        mips_code = MIPS.semantic_error
+    return mips_code
 
 
 if __name__ == "__main__":
