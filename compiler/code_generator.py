46bc3f74a12b4ba9b0ffdb0b0473f0351be3436a
seyyedAlirezaGhazanfari <59168138+seyyedAlirezaGhazanfari@users.noreply.github.com>
2022-08-07 16:36:35 +0430

    fix(cgen): Add sth for testing

--- compiler/code_generator.py
+++ compiler/code_generator.py
@@ -1,2 +1,17 @@
 def generate(code):
     pass
+
+
+if __name__ == "__main__":
+    inputfile = 'example.d'
+
+    # inputfile = '../tmp/in.d'
+    code = ""
+    with open(inputfile, "r") as input_file:
+        code = input_file.read()
+    code = generate(code)
+    print("#### code ")
+    print(code)
+
+    output_file = open("../tmp/res.mips", "w")
+    output_file.write(code)
